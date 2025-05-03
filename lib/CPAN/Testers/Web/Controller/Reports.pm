96f16f2600aa67a1be156d1dfc13866cf4b9e1cd
Doug Bell <doug@preaction.me>
2025-05-03 12:45:18 +0200

    disable recent_uploads list as too slow to generate
    Having the main page of cpantesters.org take 30 seconds to generate is
    not instilling much confidence in the system (not that confidence is
    presently deserved :p)

--- lib/CPAN/Testers/Web/Controller/Reports.pm
+++ lib/CPAN/Testers/Web/Controller/Reports.pm
@@ -19,29 +19,31 @@ List the recent uploads to CPAN and the reports received so far.
 =cut
 
 sub recent_uploads( $c ) {
-    my $recent_uploads = $c->schema->perl5->resultset('Upload')->recent(20);
-    my $rs = $c->schema->perl5->resultset('Release')->search({
-            -or => [
-                map +{ 'me.dist' => $_->dist, 'me.version' => $_->version }, $recent_uploads->all
-            ]
-        },
-        {
-            join => 'upload',
-            order_by => { -desc => 'upload.released' },
-            group_by => [qw( me.dist me.version )],
-            select => [qw( dist version uploadid upload.author upload.released), \('COUNT(*)', 'SUM(pass)', 'SUM(fail)', 'SUM(na)', 'SUM(unknown)') ],
-            as => [qw( dist version uploadid author released total pass fail na unknown )],
-        }
-    );
+    # XXX: Disabling for now as too slow
+    # my $recent_uploads = $c->schema->perl5->resultset('Upload')->recent(20);
+    # my $rs = $c->schema->perl5->resultset('Release')->search({
+    #         -or => [
+    #             map +{ 'me.dist' => $_->dist, 'me.version' => $_->version }, $recent_uploads->all
+    #         ]
+    #     },
+    #     {
+    #         join => 'upload',
+    #         order_by => { -desc => 'upload.released' },
+    #         group_by => [qw( me.dist me.version )],
+    #         select => [qw( dist version uploadid upload.author upload.released), \('COUNT(*)', 'SUM(pass)', 'SUM(fail)', 'SUM(na)', 'SUM(unknown)') ],
+    #         as => [qw( dist version uploadid author released total pass fail na unknown )],
+    #     }
+    # );
 
     $c->render( 'reports/recent_uploads',
-        items => [
-            map +{
-                $_->get_inflated_columns,
-                released => $_->upload->released->datetime( ' ' ),
-            },
-            $rs->all
-        ],
+        items => []
+        # items => [
+        #     map +{
+        #         $_->get_inflated_columns,
+        #         released => $_->upload->released->datetime( ' ' ),
+        #     },
+        #     $rs->all
+        # ],
     );
 }
 
